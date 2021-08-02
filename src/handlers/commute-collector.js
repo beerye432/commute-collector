f3f46ec04d9d3855a9d669a5b5b4d9ec91e54f1b
Brian Clark <beerye432@gmail.com>
2021-08-02 01:26:11 -0700

    google API key env variable, test request

--- src/handlers/commute-collector.js
+++ src/handlers/commute-collector.js
@@ -1,6 +1,26 @@
-/**
- * A Lambda function that returns a string.
- */
+const https = require('https');
+
+
 exports.commuteCollectorHandler = async () => {
-    console.log("worked")
+
+    const url = new URL('https://maps.googleapis.com/maps/api/distancematrix/json');
+    url.searchParams.append('units', 'imperial');
+    url.searchParams.append('departure_time', 'now');
+    url.searchParams.append('origins', '33.964915,-118.363236');
+    url.searchParams.append('destinations', '34.032860,-118.458040');
+    url.searchParams.append('key', process.env.GOOGLE_API_KEY);
+
+    https.get(url, (resp) => {
+        let data = '';
+
+        // A chunk of data has been received.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            console.log(JSON.parse(data));
+        });
+    });
 };
